537cdd013fd14f22749e60a35e6625e9b2c203bd
grigciulache <grigciulache@gmail.com>
2021-03-14 15:26:35 +0200

    Modify index

--- run.ps1
+++ run.ps1
@@ -1,8 +1,9 @@
 ﻿
 docker stop apache
 docker rm apache
-#docker build -t apache .
 
+#docker build -t apache .
 #docker run -d -p 8089:80 --name apache apache
+
 docker rmi grigciulache/httpd2:latest
 docker run -d -p 8089:80 --name apache grigciulache/httpd2:latest
