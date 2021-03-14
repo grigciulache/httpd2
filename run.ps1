eae598e38b970f1c9df06461bd24d9b04c3306e1
grigciulache <grigciulache@gmail.com>
2021-03-14 15:01:58 +0200

    Upload ps script

---
+++ run.ps1
@@ -0,0 +1,8 @@
+﻿
+docker stop apache
+docker rm apache
+#docker build -t apache .
+
+#docker run -d -p 8089:80 --name apache apache
+docker rmi grigciulache/httpd2:latest
+docker run -d -p 8089:80 --name apache grigciulache/httpd2:latest
